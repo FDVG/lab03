a10c73a24026fa42bb921bd60871fe67eb36c52a
FDVG <FDVRegistr@yandex.ru>
2020-02-12 13:36:17 +0300

    made Hello World.cpp

---
+++ homework/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+//#include <cstlib> // for system
+using namespace std;
+
+int main()
+{
+    cout << "Hello, world!" << endl;
+    //system("pause"): //for those who use MS VC
+    return 0;
+}
